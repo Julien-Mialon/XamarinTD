bd4b07fe24b6867c04e976749aa8614dc5d7b07d
Julien Mialon <mialon.julien@gmail.com>
2015-02-09 10:00:45 +0100

    Add some helpers for TD

---
+++ Helpers/HashHelper.cs
@@ -0,0 +1,28 @@
+﻿using System.Text;
+using PCLCrypto;
+
+namespace Helpers
+{
+	public static class HashHelper
+	{
+		public static string Hash(string password)
+		{
+			IHashAlgorithmProvider hasher = WinRTCrypto.HashAlgorithmProvider.OpenAlgorithm(HashAlgorithm.Sha1);
+			byte[] data = Encoding.UTF8.GetBytes(password);
+			byte[] hashed = hasher.HashData(data);
+
+			StringBuilder builder = new StringBuilder();
+			foreach (byte b in hashed)
+			{
+				string part = string.Format("{0:X}", b);
+				for (int i = part.Length; i < 2; ++i)
+				{
+					builder.Append("0");
+				}
+				builder.Append(part);
+			}
+
+			return builder.ToString().ToLowerInvariant();
+		}
+	}
+}
---
+++ Helpers/RequestHelper.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace Helpers
+{
+	class RequestHelper
+	{
+		private const int TIMEOUT_SECONDS = 30;
+		private const string USER_AGENT = "XamarinTD .NET v1.0";
+		
+	    private static readonly HttpClient _httpClient;
+
+		static RequestHelper()
+		{
+			_httpClient = new HttpClient()
+			{
+				Timeout = TimeSpan.FromSeconds(TIMEOUT_SECONDS)
+			};
+		}
+
+	    public static HttpRequestMessage SetupRequest(string endpoint, string method = "GET")
+	    {
+		    HttpMethod httpMethod = new HttpMethod(method);
+		    HttpRequestMessage request = new HttpRequestMessage(httpMethod, endpoint);
+
+		    return request;
+	    }
+
+	    public static async Task<string> ExecuteRequestAsync(HttpRequestMessage request)
+	    {
+		    HttpResponseMessage response = await _httpClient.SendAsync(request);
+
+		    response.EnsureSuccessStatusCode();
+		    string result = await response.Content.ReadAsStringAsync();
+
+		    return result;
+	    }
+	}
+}
